d0dbdcc1fb3ba1fea19d462a9ed927ba602ebe21
Valdir-Junior10 <valdir.conceicaojr@gmail.com>
2022-09-13 17:18:58 +0100

    elaboração dados pessoais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,43 +4,60 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
-    path: 'dados-pessoais',
-    loadChildren: () => import('./dados-pessoais/dados-pessoais.module').then( m => m.DadosPessoaisPageModule)
+    path: 'a',
+    loadChildren: () =>
+      import('./dados-pessoais/dados-pessoais.module').then(
+        (m) => m.DadosPessoaisPageModule
+      ),
   },
   {
     path: 'redes-sociais',
-    loadChildren: () => import('./redes-sociais/redes-sociais.module').then( m => m.RedesSociaisPageModule)
+    loadChildren: () =>
+      import('./redes-sociais/redes-sociais.module').then(
+        (m) => m.RedesSociaisPageModule
+      ),
   },
   {
     path: 'seus-estudos',
-    loadChildren: () => import('./seus-estudos/seus-estudos.module').then( m => m.SeusEstudosPageModule)
+    loadChildren: () =>
+      import('./seus-estudos/seus-estudos.module').then(
+        (m) => m.SeusEstudosPageModule
+      ),
   },
   {
     path: 'conclusao',
-    loadChildren: () => import('./conclusao/conclusao.module').then( m => m.ConclusaoPageModule)
+    loadChildren: () =>
+      import('./conclusao/conclusao.module').then((m) => m.ConclusaoPageModule),
   },
   {
     path: 'dados-pessoais-second',
-    loadChildren: () => import('./dados-pessoais-second/dados-pessoais-second.module').then( m => m.DadosPessoaisSecondPageModule)
+    loadChildren: () =>
+      import('./dados-pessoais-second/dados-pessoais-second.module').then(
+        (m) => m.DadosPessoaisSecondPageModule
+      ),
   },
   {
     path: 'seus-estudos-second',
-    loadChildren: () => import('./seus-estudos-second/seus-estudos-second.module').then( m => m.SeusEstudosSecondPageModule)
+    loadChildren: () =>
+      import('./seus-estudos-second/seus-estudos-second.module').then(
+        (m) => m.SeusEstudosSecondPageModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
