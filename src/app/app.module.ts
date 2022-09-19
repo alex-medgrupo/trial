92e41c2aadd1ecf14a46c4c22f7479d9333fedf1
alex-medgrupo <alexandre.junior@terceirizados.medgrupo.com.br>
2022-09-19 16:35:42 -0300

    animação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+// import { fancyAnimation } from '@shared/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
+    IonicModule.forRoot(/* {
+      navAnimation: fancyAnimation
+    } */),
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
