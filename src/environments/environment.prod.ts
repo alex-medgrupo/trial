e8ad15f9ee451761b0f746c948da060d27b9c9d6
Peter <github@peter.dev.br>
2022-09-19 17:14:25 -0300

    fix: prod env para gerar build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
 };
+
+// TODO: corrigir para url certa
+export const API_TRIAL =
+  'https://mstrialopcore.medgrupo.com.br/Inscricoes.svc/json';
